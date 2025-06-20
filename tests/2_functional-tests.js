0c905f25050dc0b5c83a0e54479b84c66df4f7c1
Cesar Henrique <cesaarhvb@gmail.com>
2025-06-20 19:25:56 -0300

    feat(tests): add functional tests for puzzle solving and error handling
    - Introduced tests for solving a valid puzzle string, ensuring the correct solution is returned.
    - Added a test for handling invalid characters in the puzzle string, verifying appropriate error response.
    - Updated test suite formatting for consistency.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,36 @@
 const chai = require("chai");
-const chaiHttp = require('chai-http');
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
+const { puzzlesAndSolutions } = require("../controllers/puzzle-strings.js");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  test("Solve a puzzle with valid puzzle string", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({ puzzle: puzzlesAndSolutions[0][0] })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.solution, puzzlesAndSolutions[0][1]);
+        done();
+      });
+  });
 
+  test("Solve a puzzle with invalid characters", (done) => {
+    chai
+      .request(server)
+      .post("/api/solve")
+      .send({
+        puzzle:
+          "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926c14.37.",
+      })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.error, "Invalid characters in puzzle");
+        done();
+      });
+  });
 });
-
