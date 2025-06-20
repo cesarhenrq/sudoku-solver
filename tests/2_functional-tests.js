16ccfa9636c4594d8aaad95e33534232df4e44e5
Cesar Henrique <cesaarhvb@gmail.com>
2025-06-20 20:23:43 -0300

    test(functional): update test for unsolvable puzzle scenario
    - Modified the existing test for solving a puzzle that cannot be solved to use a different puzzle string, ensuring it accurately tests the API's response for unsolvable cases.
    - The test now confirms that the API returns the correct error message when the puzzle cannot be solved.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -64,20 +64,20 @@ suite("Functional Tests", () => {
       });
   });
 
-  /* test("Solve a puzzle that cannot be solved", (done) => {
+  test("Solve a puzzle that cannot be solved", (done) => {
     chai
       .request(server)
       .post("/api/solve")
       .send({
         puzzle:
-          "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.372.",
+          "53..7....6..195....98....6.8...6...34..8..3..17...2...6.6....28....419..5....8..7",
       })
       .end((err, res) => {
         assert.equal(res.status, 200);
         assert.equal(res.body.error, "Puzzle cannot be solved");
         done();
       });
-  }); */
+  });
 
   test("Check a puzzle placement with all fields", (done) => {
     chai
